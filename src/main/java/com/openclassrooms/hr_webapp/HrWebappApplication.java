5228e8ac2e019c8a3fc3429c060457861844722b
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-01-13 19:11:00 +0100

    Fonctionnement de base - Rename de hr-webapp en hrwebapp en raison de soucis avec ide et _ - Définition de EmployeeProxy avec la méthode getEmployees permettant de   requeter le endpoint employees via RestTemplate - Affichage du résultat sur la console.

--- src/main/java/com/openclassrooms/hr_webapp/HrWebappApplication.java
+++ src/main/java/com/openclassrooms/hr_webapp/HrWebappApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.openclassrooms.hr_webapp.repository.EmployeeProxy;
+
 import lombok.Data;
 
 
@@ -18,9 +20,13 @@ public class HrWebappApplication implements CommandLineRunner {
 	@Autowired
 	private HrProperties hrProperties;
 
+	@Autowired EmployeeProxy employeeProxy;
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Propriété apiUrl: " +  hrProperties.getApiUrl());
+		System.out.println("Récupération des employés");
+		System.out.println(employeeProxy.getEmployees());
 	}
 
 	
---
+++ src/main/java/com/openclassrooms/hr_webapp/model/Employee.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.hr_webapp.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private String password;
+    
+
+}
