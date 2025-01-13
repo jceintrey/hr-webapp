39f085e6a33004ba700951b17d50e80e7599fc78
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-01-13 16:13:41 +0100

    Initial commit à partir de Spring initializr structure

---
+++ src/main/java/com/openclassrooms/hr_webapp/HrWebappApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.hr_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/hr_webapp/HrWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.hr_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
